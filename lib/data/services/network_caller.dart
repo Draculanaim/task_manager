bf3c7522285040ab0d66a86b82b0897faeb4cc83
Naim <jimkhannaim97@gmail.com>
2025-01-28 23:59:07 +0600

    network update

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class NetworkResponse {
+  final int statusCode;
+  final Map<String, dynamic>? responseData;
+  final bool isSuccess;
+  final String? errorMessage;
+
+  NetworkResponse(
+      {required this.statusCode,
+      required this.isSuccess,
+      this.responseData,
+      this.errorMessage});
+}
+
+
+class NetworkCaller {
+  static Future<NetworkResponse> getRequest(
+      {required String url, Map<String, dynamic>? params}) async {
+    try{
+      Uri uri = Uri.parse(url);
+      debugPrint('URL => $url');
+      Response response = await get(uri);
+      debugPrint('Response Code => ${response.statusCode}');
+      debugPrint('Response data => ${response.body}');
+      if (response.statusCode == 200) {
+        final decodeResponse = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            responseData: decodeResponse);
+      } else {
+        return NetworkResponse(isSuccess: false, statusCode: response.statusCode);
+      }
+    }catch(e){
+      return NetworkResponse(isSuccess: false, statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+  static Future<NetworkResponse> postRequest(
+      {required String url, Map<String, dynamic>? body}) async {
+    try{
+      Uri uri = Uri.parse(url);
+      debugPrint('URL => $url');
+      Response response = await post(uri,
+          headers: {
+        'content-type':'application/json'
+          },
+          body:jsonEncode(body));
+      debugPrint('Response Code => ${response.statusCode}');
+      debugPrint('Response data => ${response.body}');
+      if (response.statusCode == 200) {
+        final decodeResponse = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            responseData: decodeResponse);
+      } else {
+        return NetworkResponse(isSuccess: false, statusCode: response.statusCode);
+      }
+    }catch(e){
+      return NetworkResponse(isSuccess: false, statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+}
